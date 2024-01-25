176dfdb040b1fab4cb028394a418efffcd639883
Alex Alvarado <alex8m@gmail.com>
2024-01-25 02:24:48 -0600

    adding loop for pagination request, adding 1000 images limitation, parsing for better readability

--- main.py
+++ main.py
@@ -1,27 +1,34 @@
 import requests
 from bs4 import BeautifulSoup
 
-url = "https://www.freeimages.com/search/dogs"
-page = requests.get(url)
+base_url = "https://www.freeimages.com/search/dogs"
+max_images = 1000
 
+page_index = 0
+images_array = []
 
-soup = BeautifulSoup(page.content, "html.parser")
+while len(images_array) < max_images:
+    page_index += 1
+    page_url = f"{base_url}/{page_index}"
+    page = requests.get(page_url)
+    soup = BeautifulSoup(page.content, "html.parser")
 
-results = soup.find(id="content-wrapper")
+    results = soup.find(id="content-wrapper")
 
-image_elements = results.find_all("div", class_="grid-item")
+    not_contents_found_text_html = results.find_all(
+        "p", string=lambda text: "no contents yet." in text.lower())
 
+    if not_contents_found_text_html:
+        break
 
-for image_element in image_elements:
-    image = image_element.find("img", class_="grid-thumb")
-    
-    if image:
-        if image["alt"] != "iStock":
+    image_elements = results.find_all("div", class_="grid-item")
 
-            print(image["src"])
-            print("")
-        else:
-            print(image)
-            print("")
+    for image_element in image_elements:
+        image = image_element.find("img", class_="grid-thumb",
+                                   alt=lambda alt: alt and alt != "iStock")
 
-    
+        if image:
+            images_array.append(image["src"])
+
+    print(f"page index {page_index}")
+    print(f"total images: {len(images_array)}")
