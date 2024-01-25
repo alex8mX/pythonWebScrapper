8aab244bc843544ea492747d050164637ec16dad
Alex Alvarado <alex8m@gmail.com>
2024-01-25 01:40:42 -0600

    renaming files, first draft to get url images

--- main.py
+++ main.py
@@ -1,40 +1,27 @@
 import requests
 from bs4 import BeautifulSoup
 
-
-url = "https://realpython.github.io/fake-jobs/"
+url = "https://www.freeimages.com/search/dogs"
 page = requests.get(url)
+
 
 soup = BeautifulSoup(page.content, "html.parser")
 
-results = soup.find(id="ResultsContainer")
+results = soup.find(id="content-wrapper")
 
-job_elements =results.find_all("div", class_="card-content")
+image_elements = results.find_all("div", class_="grid-item")
 
-# =============================================================================
-# for job_element in job_elements:
-#     title_element = job_element.find("h2", class_="title")
-#     company_element = job_element.find("h3", class_="company")
-#     location_element = job_element.find("p", class_="location")
-#     print(title_element.text.strip())
-#     print(company_element.text.strip())
-#     print(location_element.text.strip())
-# 
-#     print()
-# =============================================================================
 
-python_jobs = results.find_all(
-    "h2", string=lambda text: "python" in text.lower())
+for image_element in image_elements:
+    image = image_element.find("img", class_="grid-thumb")
+    
+    if image:
+        if image["alt"] != "iStock":
 
-python_job_elements = [
-    h2_element.parent.parent.parent for h2_element in python_jobs
-]
+            print(image["src"])
+            print("")
+        else:
+            print(image)
+            print("")
 
-for job_element in python_job_elements:
-    links = job_element.find_all("a")
-    for link in links:
-        link_url = link["href"]
-        print(f"Apply here: {link_url}\n")
-        
-    link_url = job_element.find_all("a")[1]["href"]
-    print(f"Apply here: {link_url}\n")
+    
